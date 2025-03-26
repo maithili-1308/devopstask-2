fe137aec22f05de6a184c9c828b1c1fb65b61155
maithili-1308 <maithili.lite@aressindia.net>
2025-03-26 11:42:29 +0530

    Create log_analysis.sh

--- log_analysis.sh
+++ log_analysis.sh
@@ -15,21 +15,21 @@
 #                fi
 #
 #                # Top 5 IP addresses with the most requests
-#                echo "📌 Top 5 IP addresses with the most requests:"
+#                echo " Top 5 IP addresses with the most requests:"
 #                awk '{print $1}' "$LOG_FILE" | sort | uniq -c | sort -rn | head -5
 #
 #                echo ""
 #                # Top 5 most requested paths
-#                echo "📌 Top 5 most requested paths:"
+#                echo " Top 5 most requested paths:"
 #                awk '{print $7}' "$LOG_FILE" | sort | uniq -c | sort -rn | head -5
 #
 #                echo ""
 #                # Top 5 response status codes
-#                echo "📌 Top 5 response status codes:"
+#                echo " Top 5 response status codes:"
 #                awk '{print $9}' "$LOG_FILE" | grep -E '^[0-9]+$' | sort | uniq -c | sort -rn | head -5
 #
 #                echo ""
 #                # Top 5 user agents
-#                echo "📌 Top 5 user agents:"
+#                echo " Top 5 user agents:"
 #                awk -F'"' '{print $6}' "$LOG_FILE" | sort | uniq -c | sort -rn | head -5
 #
